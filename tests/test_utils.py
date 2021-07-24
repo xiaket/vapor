0d541c72e9be74148d1a92d96ac10dbe4a55bc94
Kai Xia <kaix+github@fastmail.com>
2021-07-24 14:27:56 +1000

    add color logger.
    Signed-off-by: Kai Xia <kaix+github@fastmail.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,9 @@
 """
 Testing helpers in utils.py
 """
-from vapor.utils import format_name
+import logging
+
+from vapor.utils import format_name, get_logger, ColorFormatter
 
 
 def test_format_name():
@@ -10,3 +12,22 @@ def test_format_name():
     assert format_name("TestStack") == "test-stack"
     assert format_name("TestAPIStack") == "test-api-stack"
     assert format_name("TestS3Stack") == "test-s3-stack"
+    assert format_name("TestCloudformationS3Stack") == "test-cloudformation-s3-stack"
+
+
+def test_color_logger(caplog, capsys):
+    logger_name = "unit"
+    logger = get_logger(logger_name)
+
+    levels = ["info", "warning", "error", "critical"]
+    for level in levels:
+        getattr(logger, level)(level)
+
+    records = caplog.record_tuples
+    lines = capsys.readouterr().out.splitlines()
+
+    for i, level in enumerate(levels):
+        logging_level = getattr(logging, level.upper())
+        assert records[i] == (logger_name, logging_level, level)
+        expected_color = ColorFormatter.FORMATS[logging_level][:8]
+        assert lines[i].startswith(expected_color)
--- vapor/utils.py
+++ vapor/utils.py
@@ -2,7 +2,9 @@
 """
 Utility functions used in vapor.
 """
+import logging
 import re
+import sys
 
 
 def format_name(name):
@@ -14,9 +16,43 @@ def format_name(name):
     However, if you want to have fine-grained control, please specify the name field in
     DeployOptions in your Stack class.
 
-    >>> format_name("TestStack") == "test-stack"
-    >>> format_name("TestAPIStack") == "test-api-stack"
-    >>> format_name("TestS3Stack") == "test-s3-stack"
+    example: format_name("TestStack") == "test-stack"
     """
-    name = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1-\2', name).lower()
+    name = re.sub("(.)([A-Z][a-z]+)", r"\1-\2", name)
+    return re.sub("([a-z0-9])([A-Z])", r"\1-\2", name).lower()
+
+
+class ColorFormatter(logging.Formatter):
+    """Logging Formatter with color."""
+
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "[%(asctime)s] %(message)s <%(filename)s:%(lineno)d>"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+        return formatter.format(record)
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setLevel(logging.DEBUG)
+
+    handler.setFormatter(ColorFormatter())
+    logger.addHandler(handler)
+    return logger
